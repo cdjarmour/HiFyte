7772996e1317132551c14679c162331f1bd6e4e4
Daniel Lee <danieltheegamer@gmail.com>
2026-06-09 20:54:24 -0500

    Chart Player Falling Notes
    Adjustable speed for falling notes prototype build.

--- Assets/PlayScene/PlayScripts/VisualManager.cs
+++ Assets/PlayScene/PlayScripts/VisualManager.cs
@@ -9,37 +9,43 @@ public class VisualManager : MonoBehaviour
     [SerializeField] AudioSource _song;
     [SerializeField] GameObject note;
     float bpm = 97;
-    float speed = 10;
-    float startPos;
+    float speed = 8;
     float beatLength;
     float timeNote;
+    float timeNeeded;
     bool playing = false;
     IReadOnlyList<Note> lane1;
     IReadOnlyList<Note> lane2;
 
     void Start() {
         beatLength = 60f / bpm;
-        _song.time = beatLength * 10;
-        _song.Play();
-        timeNote = (_song.time * 2) + beatLength;
-        playing = true;
+        playing = false;
 
-
-
-        Debug.Log("Song Time:" + _song.time);
-        Debug.Log("Time: " + timeNote);
     }
 
 
     // Update is called once per frame
-    void Update()
-    {
-        if (!playing) return;
-        note.transform.position = new Vector3(note.transform.position.x, note.transform.position.y, ((timeNote - _song.time) / beatLength) * speed);
+    void Update() {
 
-        if (((timeNote - _song.time) / beatLength) <= 0) {
-            playing = false;
-            note.transform.position = new Vector3(note.transform.position.x, transform.position.y, 0);
+        if (Input.GetKeyDown(KeyCode.P)) {
+            _song.time = beatLength * 16;
+            _song.Play();
+            timeNote = _song.time * 2;
+            playing = true;
         }
+
+        if (!playing) return;
+
+        if (_song.time >= timeNote) {
+            Debug.Log("Bruh");
+        }
+
+        timeNeeded = (timeNote - _song.time) / beatLength;
+
+        note.transform.position = new Vector3(note.transform.position.x, 0, timeNeeded * speed);
+
+    
+    
     }
+
 }
